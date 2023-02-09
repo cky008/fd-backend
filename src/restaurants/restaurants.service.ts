4af3314cf0729e182e56f8754854d3d9903ad7cb
Kaiyu Chen <greatcky83@gmail.com>
2023-02-09 20:24:24 +0000

    part of restaurants developed..

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -6,6 +6,7 @@ import {
   CreateRestaurantInput,
   CreateRestaurantOutput,
 } from './dtos/create-restaurant.dto';
+import { Category } from './entities/category.entity';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -13,6 +14,8 @@ export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
+    @InjectRepository(Category)
+    private readonly categories: Repository<Category>,
   ) {}
 
   async createRestaurant(
@@ -22,6 +25,19 @@ export class RestaurantService {
     try {
       const newRestaurant = this.restaurants.create(creatRestarurantInput);
       newRestaurant.owner = owner;
+      const categoryName = creatRestarurantInput.categoryName
+        .trim()
+        .toLowerCase();
+      const categorySlug = categoryName.replace(/ /g, '-');
+      let category = await this.categories.findOne({
+        where: { slug: categorySlug },
+      });
+      if (!category) {
+        category = await this.categories.save(
+          this.categories.create({ name: categoryName, slug: categorySlug }),
+        );
+      }
+      newRestaurant.category = category;
       await this.restaurants.save(newRestaurant);
       return { ok: true };
     } catch (error) {
