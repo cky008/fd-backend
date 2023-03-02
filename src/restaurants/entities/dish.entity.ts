d6267a2c0b069e6d57213553ff31e52a47b72a08
Kaiyu Chen <greatcky83@gmail.com>
2023-03-02 17:15:35 +0000

    update dish entity and createDish.

--- src/restaurants/entities/dish.entity.ts
+++ src/restaurants/entities/dish.entity.ts
@@ -1,10 +1,32 @@
-import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { IsNumber, IsString, Length } from 'class-validator';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Column, Entity, ManyToOne, RelationId } from 'typeorm';
 import { Restaurant } from './restaurant.entity';
 
-@InputType('DishInpuType', { isAbstract: true })
+@InputType('DishChoiceInputType', { isAbstract: true })
+@ObjectType()
+export class DishChoice {
+  @Field(() => String)
+  name: string;
+  @Field(() => Number, { nullable: true })
+  extra?: number;
+}
+
+@InputType('DishOptionInputType', { isAbstract: true })
+@ObjectType()
+export class DishOption {
+  @Field(() => String)
+  name: string;
+
+  @Field(() => [DishChoice], { nullable: true })
+  choices?: DishChoice[];
+
+  @Field(() => Number, { nullable: true })
+  extra?: number;
+}
+
+@InputType('DishInputType', { isAbstract: true })
 @ObjectType()
 @Entity()
 export class Dish extends CoreEntity {
@@ -14,20 +36,20 @@ export class Dish extends CoreEntity {
   @Length(5)
   name: string;
 
-  @Column()
-  @Field(() => Int)
+  @Column({ type: 'real' })
+  @Field(() => Number)
   @IsNumber()
   price: number;
 
-  @Column()
-  @Field(() => String)
+  @Column({ nullable: true })
+  @Field(() => String, { nullable: true })
   @IsString()
   photo: string;
 
   @Column()
-  @Field()
+  @Field(() => String)
   @IsString()
-  @Length(10, 140)
+  @Length(5, 140)
   description: string;
 
   @ManyToOne(() => Restaurant, (restaurant) => restaurant.menu, {
@@ -38,4 +60,8 @@ export class Dish extends CoreEntity {
 
   @RelationId((dish: Dish) => dish.restaurant)
   restaurantId: number;
+
+  @Column({ type: 'json', nullable: true })
+  @Field(() => [DishOption], { nullable: true })
+  options?: DishOption[];
 }
--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -2,16 +2,26 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TypeOrmCustomModule } from 'src/db/typeorm-custom.module';
 import { Category } from './entities/category.entity';
+import { Dish } from './entities/dish.entity';
 import { Restaurant } from './entities/restaurant.entity';
 import { CategoryRepository } from './repositories/category.repository';
-import { CategoryResolver, RestaurantResolver } from './restaurants.resolver';
+import {
+  CategoryResolver,
+  DishResolver,
+  RestaurantResolver,
+} from './restaurants.resolver';
 import { RestaurantService } from './restaurants.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Restaurant, Category]),
+    TypeOrmModule.forFeature([Restaurant, Category, Dish]),
     TypeOrmCustomModule.forCustomRepository([CategoryRepository]),
   ],
-  providers: [RestaurantResolver, CategoryResolver, RestaurantService],
+  providers: [
+    RestaurantResolver,
+    CategoryResolver,
+    DishResolver,
+    RestaurantService,
+  ],
 })
 export class RestaurantsModule {}
