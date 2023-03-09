3e3efa6dd6058417815511b335655f00abf63a6e
Kaiyu Chen <greatcky83@gmail.com>
2023-03-09 23:00:42 +0000

    update createPayment.

--- src/payments/entities/payment.entity.ts
+++ src/payments/entities/payment.entity.ts
@@ -1,27 +1,29 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, Int, InputType } from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entity';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
 import { Entity, Column, ManyToOne, RelationId } from 'typeorm';
 
+@InputType({ isAbstract: true })
 @ObjectType()
 @Entity()
 export class Payment extends CoreEntity {
-  @Field(() => Int)
   @Column()
-  transactionId: number;
+  @Field(() => String)
+  transactionId: string;
 
-  @Field(() => User)
   @ManyToOne(() => User, (user) => user.payments)
+  @Field(() => User)
   user: User;
 
   @RelationId((payment: Payment) => payment.user)
   userId: number;
 
-  @Field(() => Restaurant)
   @ManyToOne(() => Restaurant)
+  @Field(() => Restaurant)
   restaurant: Restaurant;
 
+  @Field(() => Int)
   @RelationId((payment: Payment) => payment.restaurant)
   restaurantId: number;
 }
